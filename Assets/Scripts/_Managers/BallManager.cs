9d5d7b92889e8274fc877f4ca0b5f80e6c2dd985
Meng-Hsin Tung <itswindtw@gmail.com>
2015-11-04 20:08:59 -0500

    Prepare for multiple types of AI balls
    * A script to switch different balls with different parameters

--- Assets/Scripts/_Managers/BallManager.cs
+++ Assets/Scripts/_Managers/BallManager.cs
@@ -4,22 +4,23 @@ using System.Collections;
 public class BallManager : MonoBehaviour
 {
 	static public BallManager instance;
-	public GameObject ballTemplate;
+	public GameObject[] ballTemplates;
 	public int numOfBalls = 0;
 	public Transform ballSpawnPoint;
 	public float minSpawnInterval = 3f;
 
 	bool readyForSpawning = true;
+	int ballTemplateIdx = 0;
 
 	void Awake ()
 	{
 		instance = this;
 	}
 
-	void FixedUpdate ()
+	void Update ()
 	{
 		if (readyForSpawning && Input.GetButtonUp ("SpawnBall")) {
-			GameObject newBall = Instantiate (ballTemplate, ballSpawnPoint.position + transform.up * 0.5f, new Quaternion (15, 0, 0, 0)) as GameObject;
+			GameObject newBall = Instantiate (ballTemplates [ballTemplateIdx], ballSpawnPoint.position + transform.up * 0.5f, new Quaternion (15, 0, 0, 0)) as GameObject;
 			newBall.transform.parent = transform;
 			numOfBalls += 1;
 
@@ -29,7 +30,7 @@ public class BallManager : MonoBehaviour
 
 		// FIXME: testing code. remove this after milestone 3
 		if (readyForSpawning && Input.GetButtonUp ("AITest")) {
-			GameObject newBall = Instantiate (ballTemplate, GameObject.Find ("AISpawningPoint").transform.position + transform.up * 0.5f, new Quaternion (15, 0, 0, 0)) as GameObject;
+			GameObject newBall = Instantiate (ballTemplates [ballTemplateIdx], GameObject.Find ("AISpawningPoint").transform.position + transform.up * 0.5f, new Quaternion (15, 0, 0, 0)) as GameObject;
 			newBall.transform.parent = transform;
 			numOfBalls += 1;
 
@@ -37,6 +38,18 @@ public class BallManager : MonoBehaviour
 
 			readyForSpawning = false;
 			Invoke ("GetReadyForSpawning", minSpawnInterval);
+		}
+
+		// FIXME: same as above...
+		if (Input.GetKeyUp ("1")) {
+			Debug.Log ("Set BallTemplate as MultiBall 1");
+			ballTemplateIdx = 0;
+		} else if (Input.GetKeyUp ("2")) {
+			Debug.Log ("Set BallTemplate as MultiBall 2");
+			ballTemplateIdx = 1;
+		} else if (Input.GetKeyUp ("3")) {
+			Debug.Log ("Set BallTemplate as MultiBall 3");
+			ballTemplateIdx = 2;
 		}
 	}
 
