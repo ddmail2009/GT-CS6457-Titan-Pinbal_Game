8eeb862bd87b507f52dc1afaef240848f25ea918
rain4u <skywalker.alone@gmail.com>
2015-11-07 16:00:12 -0500

    Integrate Portal

---
+++ Assets/Scripts/Portal/PortalController.cs
@@ -0,0 +1,82 @@
+﻿/**
+ * Titan
+ * 
+ * PoHsien Wang
+ * 
+ * Protal Controller Script
+ * @param objectTag objects with which tag are allowed to enter
+ * @param enterLocalDirection only allow entering the portal in this direction(local representation)
+ * @param linkPortals linkedPortals
+ * 
+ * @param outDelay how long should the ball stays in its buffer
+ * @param outSpeed the speed of the ball when launching from portal
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class PortalController : MonoBehaviour
+{
+	public string objectTag = "Ball";
+	public Vector3 enterLocalDirection = Vector3.forward;
+	public GameObject[] linkPortals = null;
+
+	public float outDelay = 5f;
+	public float outSpeed = 20f;
+
+
+	private Vector3 enterWorldDirection;
+	private ArrayList ballBuffer = new ArrayList ();
+	void Start ()
+	{
+		enterWorldDirection = transform.TransformDirection (enterLocalDirection);
+	}
+
+	void OnTriggerEnter (Collider col)
+	{
+		if (col.tag.Equals (objectTag)) {
+			Vector3 vin = col.attachedRigidbody.GetRelativePointVelocity (transform.position);
+			float t = Vector3.Dot (vin, enterWorldDirection);
+			if (!Mathf.Approximately (t, 0f) && t < 0) {
+				GameObject portal = null;
+				for (int i=0; i<linkPortals.Length; i++)
+					if ((portal = linkPortals [Random.Range (0, linkPortals.Length)]) != null)
+						break;
+				if (portal != null) {
+					col.gameObject.SetActive (false);
+					GetComponent<PortalSoundManager> ().ballEnter ();
+
+					portal.GetComponent<PortalController> ().addBalltoBuffer (col.gameObject);
+				}
+			}
+		}
+	}
+
+
+	void addBalltoBuffer (GameObject ball)
+	{
+		ballBuffer.Add (ball);
+		GetComponent<PortalSoundManager> ().ballBuffer ();
+
+		// out degree -60/60 with out portal forward direction
+		Vector3 linkPortalsF = transform.TransformDirection (Vector3.forward);
+		Vector3 linkPortalsH = transform.TransformDirection (Vector3.right);
+		float forwardSpeed = Random.Range (0.5f, 1f) * outSpeed;
+		float horizontalSpeed = Mathf.Sqrt (outSpeed * outSpeed - forwardSpeed * forwardSpeed) * Random.Range (-1, 2);
+
+		ball.GetComponent<Rigidbody> ().velocity = linkPortalsF * forwardSpeed + linkPortalsH * horizontalSpeed;
+		ball.transform.position = transform.position + linkPortalsF;
+
+		Invoke ("FireBall", outDelay);
+	}
+
+	// Fire ball through the portal
+	void FireBall ()
+	{
+		GameObject ball = (GameObject)ballBuffer [0];
+		ballBuffer.RemoveAt (0);
+		ball.SetActive (true);
+		GetComponent<PortalSoundManager> ().ballLaunch ();
+		GetComponent<PortalSoundManager> ().setIdle (ballBuffer.Count == 0);
+	}
+}
---
+++ Assets/Scripts/Portal/PortalSoundManager.cs
@@ -0,0 +1,71 @@
+﻿/**
+ * Titan
+ * 
+ * PoHsien Wang
+ * 
+ * Protal Sound Manager Script
+ * Note: Portal must have at least two AudioSources(one for environmental Sound, one for sound effect)
+ * @param IdleSound sound when no balls in buffer
+ * @param BallWaiting sound when balls in buffer
+ * @param BallLaunching sound when launching balls
+ * @param BallEntering sound when balls enter portal
+ * @param backgroundVolume volume of idle & waiting sound
+ * @param effectVolume volume of launching & entering sound
+ * 
+ * public method
+ * @method setIdle(bool) switch to idle sound or not
+ * @method ballLaunch() play BallLaunching
+ * @method ballEnter() play BallEntering
+ * @method ballBuffer() play BallWaiting
+ */
+
+using UnityEngine;
+
+public class PortalSoundManager : MonoBehaviour
+{
+	public AudioClip IdleSound;
+	public AudioClip BallWaiting;
+
+	public AudioClip BallLaunching;
+	public AudioClip BallEntering;
+	public float backgroundVolume = 0.2f;
+	public float effectVolume = 1f;
+
+
+	private AudioSource[]sources;
+	void Start ()
+	{
+		sources = GetComponents<AudioSource> ();
+		backgroundSoundSet (IdleSound);
+	}
+
+	void backgroundSoundSet (AudioClip clip)
+	{
+		sources [0].Stop ();
+		sources [0].clip = clip;
+		sources [0].loop = true;
+		sources [0].volume = backgroundVolume;
+		sources [0].Play ();
+	}
+
+	public void setIdle (bool idle)
+	{
+		if (idle)
+			backgroundSoundSet (IdleSound);
+	}
+
+	public void ballLaunch ()
+	{
+		sources [1].PlayOneShot (BallLaunching);
+	}
+
+	public void ballBuffer ()
+	{
+		backgroundSoundSet (BallWaiting);
+	}
+
+	public void ballEnter ()
+	{
+		sources [1].PlayOneShot (BallEntering);
+	}
+}
