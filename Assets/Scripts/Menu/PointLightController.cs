c95623706ba7f55d7d11994c271e6f9d25106650
Meng-Hsin Tung <itswindtw@gmail.com>
2015-11-12 17:22:18 -0500

    Menu scene tweak, preparing for credits scene

---
+++ Assets/Scripts/Menu/PointLightController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointLightController : MonoBehaviour
+{
+	public float speed = 0.01f;
+	public float maxRange = 20.0f, minRange = 10.0f;
+
+	bool isIncreasing = false;
+	Light light;
+		
+	void Awake ()
+	{
+		light = GetComponent <Light> ();
+	}
+
+	void Update ()
+	{
+		light.range += (isIncreasing) ? speed : -speed;
+
+		if ((isIncreasing && light.range > maxRange) || (!isIncreasing && light.range < minRange)) {
+			isIncreasing = !isIncreasing;
+
+			if (!isIncreasing) {
+				float r = Random.Range (0, 1f), g = Random.Range (0, 1f), b = Random.Range (0, 1f);
+				float scale = 3.0f / (r + g + b);
+
+				light.color = new Color (r * scale, g * scale, b * scale);
+			}
+		}
+	}
+}
