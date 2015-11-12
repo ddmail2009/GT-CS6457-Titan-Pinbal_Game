f6ce7c0f2ccbf547c6e227d4f3149b93719c2caa
Meng-Hsin Tung <itswindtw@gmail.com>
2015-11-11 21:08:28 -0500

    Menu scene draft

---
+++ Assets/Scripts/Menu/OrbitCamera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OrbitCamera : MonoBehaviour
+{
+	public Transform orbitCenter;
+	public float orbitDistance;
+	public float orbitSpeed;
+
+	void LateUpdate ()
+	{
+		Vector3 towardOrbitCenter = orbitCenter.position - transform.position;
+		towardOrbitCenter += -transform.right * orbitSpeed;
+
+		towardOrbitCenter = towardOrbitCenter.normalized * orbitDistance;
+
+		transform.position = orbitCenter.position - towardOrbitCenter;
+		transform.LookAt (orbitCenter.position);
+	}
+}
