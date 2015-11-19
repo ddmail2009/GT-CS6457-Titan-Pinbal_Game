58c1cf707bf6ba23b2bfc1a6d1641f21961c0dd1
Meng-Hsin Tung <itswindtw@gmail.com>
2015-11-19 16:53:13 -0500

    MissleBallAI normal state gravity

--- Assets/Scripts/MissileBallAI.cs
+++ Assets/Scripts/MissileBallAI.cs
@@ -113,7 +113,7 @@ public class MissileBallAI : MonoBehaviour
 	
 	void normal_update ()
 	{
-		rig.AddForce (Vector3.down * normalGravity, ForceMode.Acceleration);
+		rig.AddForce (-Vector3.forward * normalGravity, ForceMode.Acceleration);
 		
 		// Conditions to switch to returning state.
 		float currDistToPlayer = Vector3.Distance (playerTransform.position, transform.position);
