32353c98bacf6e1489d33196e9561bc0e11be223
Zizheng Wu <me@zizhengwu.com>
2015-11-20 00:13:06 -0500

    bomb will detect colliders
    Player gets HP deducted
    PlayerItem will be destroyed

--- Assets/Scripts/Bomb/Explosion.cs
+++ Assets/Scripts/Bomb/Explosion.cs
@@ -2,6 +2,22 @@
 using System.Collections;
 
 public class Explosion : MonoBehaviour {
+    void Start () {
+        Vector3 center = transform.position;
+        float radius = 5.0f;
+        Collider[] hitColliders = Physics.OverlapSphere(center, radius);
+
+        foreach (Collider collider in hitColliders)
+        {
+            Debug.Log(collider.gameObject.tag);
+            if (collider.gameObject.tag == "Player") {
+                PlayerHealthManager.instance.Heal(-10);
+            }
+            else if (collider.gameObject.tag == "PlayerItem") {
+                Destroy(collider.gameObject);
+            }
+        }
+    }
 
 	void Update () {
 		
