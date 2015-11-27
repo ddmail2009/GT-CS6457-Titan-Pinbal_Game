c6b46ef89297cf2b2360545fc7e6795192b07f26
Meng-Hsin Tung <itswindtw@gmail.com>
2015-11-27 14:44:28 -0500

    Integrate PlayerItem and Powerpellet

---
+++ Assets/Scripts/PlayerItem/PowerPelletManager.cs
@@ -0,0 +1,52 @@
+﻿/**
+ * Team Titan
+ * 
+ * Meng-Hsin Tung
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class PowerPelletManager : MonoBehaviour
+{
+	public static PowerPelletManager instance;
+	public AudioClip clip;
+	public bool isEnabled = false;
+
+	float _timer = 0f, _duration = 0f;
+	ParticleSystem effect;
+	
+	public void Enable (float duration)
+	{
+		AudioSource.PlayClipAtPoint (clip, transform.position, 1.0f);
+		_duration += duration;
+
+		isEnabled = true;
+		effect.Play ();
+	}
+
+	void Disable ()
+	{
+		_duration = 0f;
+		_timer = 0f;
+		isEnabled = false;
+		effect.Stop ();
+	}
+
+	void Awake ()
+	{
+		instance = this;
+		effect = transform.Find ("PowerPelletEffect").GetComponent<ParticleSystem> ();
+	}
+	
+	void Update ()
+	{
+		if (isEnabled) {
+			_timer += Time.deltaTime;
+
+			if (_timer > _duration) {
+				Disable ();
+			}
+		}
+	}
+}
