dc85857f29a2e2149ce3b5398c1903ef75a95fc7
Meng-Hsin Tung <itswindtw@gmail.com>
2015-11-30 22:03:57 -0500

    Fill missing BallIgnore scripts

---
+++ Assets/Scripts/Ball/BallIgnore.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallIgnore : MonoBehaviour
+{
+	void Awake ()
+	{
+		Physics.IgnoreCollision (GetComponent<Collider> (), GameObject.Find ("PlayerWall").GetComponent<Collider> ());
+	}
+}
