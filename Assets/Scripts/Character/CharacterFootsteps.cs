18030af9f74f7d91c72b193dc5900e7778471ca8
rain4u <skywalker.alone@gmail.com>
2015-12-02 18:48:00 -0500

    Event-driven footstep

---
+++ Assets/Scripts/Character/CharacterFootsteps.cs
@@ -0,0 +1,30 @@
+﻿/**
+ * Titan
+ *
+ * Xiaoyu Chen
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class CharacterFootsteps : MonoBehaviour
+{
+	public AudioClip stepClip;
+
+	AudioSource aud;
+
+	public void Awake ()
+	{
+		aud = GetComponent<AudioSource> ();
+	}
+
+	public void LeftFootstep ()
+	{
+		aud.PlayOneShot (stepClip);
+	}
+
+	public void RightFootstep ()
+	{
+		aud.PlayOneShot (stepClip);
+	}
+}
