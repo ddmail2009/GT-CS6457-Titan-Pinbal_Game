d8f8ab73346bc0960cc08c5178e2e0d8b48b13ee
Meng-Hsin Tung <itswindtw@gmail.com>
2015-12-02 19:44:34 -0500

    Integrate GameMenu

---
+++ Assets/Scripts/Menu/GameMenuController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameMenuController : MonoBehaviour
+{
+	bool isOpened = false;
+	GameObject canvas;
+	float savedTimeScale;
+
+	ThirdPersonCameraControllerBeta playerCamera;
+
+	public void LoadLevel (string levelName)
+	{
+		Application.LoadLevel (levelName);
+	}
+
+	public void ExitGame ()
+	{
+		Application.Quit ();
+	}
+
+	void Awake ()
+	{
+		playerCamera = GameObject.FindWithTag ("ThirdPersonCamera").GetComponent<ThirdPersonCameraControllerBeta> ();
+
+		canvas = transform.GetChild (0).gameObject;
+		savedTimeScale = Time.timeScale;
+	}
+
+	void Update ()
+	{
+		if (Input.GetButtonUp ("Esc")) {
+			isOpened = !isOpened;
+
+			canvas.gameObject.SetActive (isOpened);
+			Cursor.visible = isOpened;
+			playerCamera.enabled = !isOpened;
+
+			if (isOpened) {
+				savedTimeScale = Time.timeScale;
+				Time.timeScale = 0;
+			} else {
+				Time.timeScale = savedTimeScale;
+			}
+		}
+	}
+}
